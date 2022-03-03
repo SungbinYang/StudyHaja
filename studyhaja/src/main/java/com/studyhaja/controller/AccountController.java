44461b5da7b465a4c2175250b05fbafcb14e02ab
SungbinYang <ysb05222@naver.com>
2022-03-03 21:31:49 +0900

    회원 가입: 폼 서브밋 검증

--- studyhaja/src/main/java/com/studyhaja/controller/AccountController.java
+++ studyhaja/src/main/java/com/studyhaja/controller/AccountController.java
@@ -2,9 +2,18 @@ package com.studyhaja.controller;
 
 import com.studyhaja.domain.account.Account;
 import com.studyhaja.domain.account.SignUpForm;
+import com.studyhaja.domain.account.SignUpFormValidator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 /**
  * packageName : com.studyhaja.controller
@@ -19,12 +28,30 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUp(Model model, Account account) {
         model.addAttribute(new SignUpForm());
 
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) { // 에러가 있는 경우
+            return "account/sign-up";
+        }
+
+        //TODO:  회원가입 처리
+        return "redirect:/";
     }
 }
---
+++ studyhaja/src/main/java/com/studyhaja/repository/AccountRepository.java
@@ -0,0 +1,24 @@
+package com.studyhaja.repository;
+
+import com.studyhaja.domain.account.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * packageName : com.studyhaja.repository
+ * fileName : AccountRepository
+ * author : rovert
+ * date : 2022/03/03
+ * description :
+ * ===========================================================
+ * DATE 			AUTHOR			 NOTE
+ * -----------------------------------------------------------
+ * 2022/03/03       rovert         최초 생성
+ */
+
+@Transactional(readOnly = true)
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+}
