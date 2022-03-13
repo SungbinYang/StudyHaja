9472e5b9ef70ad2442b25d1f08a538594b016764
SungbinYang <ysb05222@naver.com>
2022-03-13 18:18:09 +0900

    스터디 설정 - 상태 변경

--- studyhaja/src/main/java/com/studyhaja/service/study/StudyService.java
+++ studyhaja/src/main/java/com/studyhaja/service/study/StudyService.java
@@ -36,23 +36,18 @@ public class StudyService {
     public Study createNewStudy(Study study, Account account) {
         Study newStudy = studyRepository.save(study);
         newStudy.addManager(account);
-
         return newStudy;
     }
 
     public Study getStudyToUpdate(Account account, String path) {
         Study study = this.getStudy(path);
-
         checkIfManager(account, study);
-
         return study;
     }
 
     public Study getStudy(String path) {
-        Study study = this.studyRepository.findByPath(path);
-
+        Study study = studyRepository.findByPath(path);
         checkIfExistingStudy(path, study);
-
         return study;
     }
 
@@ -89,24 +84,45 @@ public class StudyService {
     }
 
     public Study getStudyToUpdateTag(Account account, String path) {
-        Study study = studyRepository.findAccountWithTagsByPath(path);
+        Study study = studyRepository.findStudyWithTagsByPath(path);
         checkIfExistingStudy(path, study);
         checkIfManager(account, study);
-
         return study;
     }
 
     public Study getStudyToUpdateZone(Account account, String path) {
-        Study study = studyRepository.findAccountWithZonesByPath(path);
+        Study study = studyRepository.findStudyWithZonesByPath(path);
         checkIfExistingStudy(path, study);
         checkIfManager(account, study);
-
         return study;
+    }
+
+    public Study getStudyToUpdateStatus(Account account, String path) {
+        Study study = studyRepository.findStudyWithManagersByPath(path);
+        checkIfExistingStudy(path, study);
+        checkIfManager(account, study);
+        return study;
+    }
+
+    public void publish(Study study) {
+        study.publish();
+    }
+
+    public void close(Study study) {
+        study.close();
+    }
+
+    public void startRecruit(Study study) {
+        study.startRecruit();
+    }
+
+    public void stopRecruit(Study study) {
+        study.stopRecruit();
     }
 
     private void checkIfManager(Account account, Study study) {
         if (!account.isManagerOf(study)) {
-            throw new AccessDeniedException("해당 기능ㅇ을 사용할 수 없습니다.");
+            throw new AccessDeniedException("해당 기능을 사용할 수 없습니다.");
         }
     }
 
