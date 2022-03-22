0486f057351aa4afe2e56331c89d1a37bbf28783
SungbinYang <ysb05222@naver.com>
2022-03-22 21:14:55 +0900

    알림 아이콘 변경

---
+++ studyhaja/src/main/java/com/studyhaja/infra/config/WebConfig.java
@@ -0,0 +1,41 @@
+package com.studyhaja.infra.config;
+
+import com.studyhaja.modules.notification.NotificationInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.StaticResourceLocation;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * packageName : com.studyhaja.infra.config
+ * fileName : WebConfig
+ * author : rovert
+ * date : 2022/03/22
+ * description :
+ * ===========================================================
+ * DATE 			AUTHOR			 NOTE
+ * -----------------------------------------------------------
+ * 2022/03/22       rovert         최초 생성
+ */
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final NotificationInterceptor notificationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        List<String> staticResourcesPath = Arrays.stream(StaticResourceLocation.values())
+                .flatMap(StaticResourceLocation::getPatterns).collect(Collectors.toList());
+
+        staticResourcesPath.add("/node_modules/**");
+
+        registry.addInterceptor(notificationInterceptor).excludePathPatterns(staticResourcesPath);
+    }
+}
--- studyhaja/src/main/java/com/studyhaja/modules/notification/NotificationRepository.java
+++ studyhaja/src/main/java/com/studyhaja/modules/notification/NotificationRepository.java
@@ -1,5 +1,6 @@
 package com.studyhaja.modules.notification;
 
+import com.studyhaja.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -15,4 +16,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    long countByAccountAndChecked(Account account, boolean checked);
 }
